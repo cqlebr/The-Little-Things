1d2b7d0b14695b589678728fd377e0d1d4ac3ae5
Caleb <cqlebrg@gmail.com>
2024-09-29 20:26:11 -0500

    initialized

---
+++ src/client/java/project/thelittlethings/TheLittleThingsClient.java
@@ -0,0 +1,10 @@
+package project.thelittlethings;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class TheLittleThingsClient implements ClientModInitializer {
+	@Override
+	public void onInitializeClient() {
+		// This entrypoint is suitable for setting up client-specific logic, such as rendering.
+	}
+}
